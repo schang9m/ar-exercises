c9ff075442f550681cfec405f97f1ac85853f272
Sampson Chang <sampson.chang@gmail.com>
2024-08-19 20:28:23 -0700

    added 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Stores < ActiveRecord::Base
+  burnaby = Stores.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true)
+  richmond = Stores.create(name: "Richmond", annual_revenue: "1260000", mens_apparel: false, womens_apparel: true)
+  gastown = Stores.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true, womens_apparel: false)
+end
